2e5b5d28854505ed2db043e49ecdb2876fc8a5c2
Daniel Park <parkdk96@gmail.com>
2019-11-07 11:04:36 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your functions here
+const map = (arr, fn) => {
+  const newArr = [];
+  for (const item of arr) {
+    newArr.push(fn(item));
+  }
+  return newArr;
+}
+
+function reduce(array, applyFunction, startPoint){
+  let i = 0;
+  let accumulatingVariable = 0;
+  if (startPoint !== undefined){
+      i = 0;
+      accumulatingVariable = startPoint;
+  } else {
+      accumulatingVariable = array[0];
+      i = 1;
+  }
+  for(; i<array.length; i++){
+      accumulatingVariable = applyFunction(accumulatingVariable, array[i])
+  }
+  return accumulatingVariable;
+}  
